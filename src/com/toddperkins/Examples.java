4018a863875e734c766ae38ef5809c6d7e3a4583
Mike Knoles <mknoles78@gmail.com>
2021-11-14 20:21:46 -0500

    Commit and push!

--- src/com/toddperkins/Examples.java
+++ src/com/toddperkins/Examples.java
@@ -3,8 +3,8 @@ package com.toddperkins;
 public class Examples {
 
 	public static void main(String[] args) {
-	// TODO Auto-generated method stub
 
+	// TODO Auto-generated method stub
 	public void example() {
 
 	}
